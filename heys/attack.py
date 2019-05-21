015729ebb3d0455f4366f3ec339fa1f376a2f6c4
Anton Smirnov <17990405+pxl-th@users.noreply.github.com>
2019-05-21 21:39:08 +0300

    Close #7: Finalize

--- heys/attack.py
+++ heys/attack.py
@@ -1,12 +1,12 @@
-from collections import (
-    Counter,
-    defaultdict,
-)
+from collections import Counter
 from os.path import isfile
-from typing import Dict
 from pickle import (
     dump,
     load,
+)
+from typing import (
+    Dict,
+    Tuple,
 )
 
 from numpy import (
@@ -27,87 +27,95 @@ __all__ = [
 
 
 def attack(
-        alphas: ndarray,
-        approximations_file: str,
-        keys_file: str,
-        approximations_number: int = 300,
-        probability_threshold: float = 1e-5,
-        processes_number: int = 1,
-        top_keys: int = 100,
-):
-    """
+    heys: Heys,
+    data: Tuple[ndarray, ndarray],
+    approximations: Dict[int, Dict[int, float]],
+    max_approximations_number: int,
+    probability_threshold: float,
+    processes_number: int,
+    top_keys: int,
+) -> Tuple[Dict[int, Dict[int, float]], Dict[int, int]]:
+    r"""
     Perform an attack on first round-key of SPN block cipher.
 
     Args:
-        alphas ((N, )ndarray[uint16]):
-            Initial values for `branch-and-bounds` algorithm.
-        approximations_file (str):
-            Filepath, where founded approximations will be saved.
-        keys_file (str):
-            File, where to save founded key-candidates.
-        approximations_number (int):
-            Number of approximations, that algorithm should find
-            to stop.
-            If for the given `probability_threshold` algorithm found
-            less than `approximations_number` number of approximations,
-            then you should decrease `probability_threshold`
-            and start again.
+        heys (:class:`Heys`):
+            Instance of :class:`Heys` from which round functions
+            will be taken.
+        data (tuple[(M, ) ndarray[uint16], (M, ) ndarray[uint16]]):
+            Tuple where the first element is the array opentexts
+            and the second element is the array of ciphertexts,
+            corresponding to the opentexts.
+        approximations (dict[int, dict[int, float]]):
+            - int:
+                Initial `alpha` value.
+            - dict[int, float]:
+                If dictionaries has less than `max_approximations_number`
+                approximations, then algorithm will try to find more
+                approximations using :meth:`branch_bound` algorithm.
+
+                - int:
+                    Founded `beta` value for the initial `alpha` value.
+                - float:
+                    Probability :math:`p` of linear
+                    approximation (`alpha`, `beta`),
+                    for which holds :math:`p \ge p^*`,
+                    where :math:`p^*` is the `probability_threshold`.
+        max_approximations_number (int):
+            Amount of approximations that :meth:`branch_bounds` algorithm
+            should find before stopping.
+            **Note**, if for the given `probability_threshold`
+            algorithm found less than `max_approximations_number`
+            number of approximations, then you should
+            decrease `probability_threshold` and start it again.
         probability_threshold (float):
             Probability threshold which determines,
             what approximations will be selected.
             The higher the value the faster (but more inaccurate)
             algorithm is, and vise-versa, the lower the value,
-            the more robust it is, but will require much more computations.
+            the more robust it is (but will require much more computations).
         processes_number (int):
             Number of processors to use in :meth:`m2` algorithm.
         top_keys (int):
             Number of key-candidates to select in :meth:`m2` algorithm
             that have highest statistics.
-    """
-    heys_keys = array(
-        [0xff2c, 0x1488, 0xa23f, 0xe323, 0x1444, 0x2012, 0xeaa],
-        dtype="uint16",
-    )
-    heys = Heys(sbox=S_BOX, keys=heys_keys)
-    inputs = arange(start=0, stop=3000, dtype="uint16")
-    ciphertexts = heys.encrypt(message=inputs)
 
+    Returns:
+        dict[int, int]:
+            - int:
+                Key candidate.
+            - int:
+                Number of times, key candidate got in `top_keys` list.
+                The higher the number, the higher the chances of it
+                being a real key.
+    """
     approximations = calculate_approximations(
         heys=heys,
-        alphas=alphas,
-        approximations_file=approximations_file,
-        approximations_number=approximations_number,
+        approximations=approximations,
+        max_approximations_number=max_approximations_number,
         probability_threshold=probability_threshold,
     )
-
     key_candidates = m2(
         heys=heys,
-        inputs=inputs,
-        ciphertexts=ciphertexts,
+        data=data,
         approximations=approximations,
         processes_number=processes_number,
         top_keys=top_keys,
     )
-
-    keys_counter = Counter(key_candidates)
-    print(keys_counter)
-    with open(keys_file, "wb") as f:
-        dump(keys_counter, f)
+    return approximations, dict(Counter(key_candidates))
 
 
 def calculate_approximations(
-        heys: Heys,
-        alphas: ndarray,
-        approximations_file: str,
-        approximations_number: int,
-        probability_threshold: float,
+    heys: Heys,
+    approximations: Dict[int, Dict[int, float]],
+    max_approximations_number: int,
+    probability_threshold: float,
 ) -> Dict[int, Dict[int, float]]:
     r"""
     Find linear approximations for the SPN
     using :meth:`branch_bound` algorithm.
     If `heys` has :math:`r` rounds, then founded approximations are for
     :math:`r - 1` rounds, because we attack first round key.
-
     Linear approximation is a pair :math:`(\alpha, \beta)`, such that
 
     .. math::
@@ -116,23 +124,34 @@ def calculate_approximations(
     Args:
         heys (Heys):
             Cipher from which `sbox` and number of `rounds` is taken.
-        alphas ((N, )ndarray[uint16]):
-            Initial values for `branch-and-bounds` algorithm.
-        approximations_file (str):
-            Filepath, where founded approximations will be saved.
-        approximations_number (int):
-            Number of approximations, that algorithm should find
-            to stop.
-            If for the given `probability_threshold` algorithm found
-            less than `approximations_number` number of approximations,
-            then you should decrease `probability_threshold`
-            and start again.
+        approximations (dict[int, dict[int, float]]):
+            - int:
+                Initial `alpha` value.
+            - dict[int, float]:
+                If dictionaries has less than `max_approximations_number`
+                approximations, then algorithm will try to find more
+                approximations using :meth:`branch_bound` algorithm.
+
+                - int:
+                    Founded `beta` value for the initial `alpha` value.
+                - float:
+                    Probability :math:`p` of linear
+                    approximation (`alpha`, `beta`),
+                    for which holds :math:`p \ge p^*`,
+                    where :math:`p^*` is the `probability_threshold`.
+        max_approximations_number (int):
+            Amount of approximations that :meth:`branch_bounds` algorithm
+            should find before stopping.
+            **Note**, if for the given `probability_threshold`
+            algorithm found less than `max_approximations_number`
+            number of approximations, then you should
+            decrease `probability_threshold` and start it again.
         probability_threshold (float):
             Probability threshold which determines,
             what approximations will be selected.
             The higher the value the faster (but more inaccurate)
             algorithm is, and vise-versa, the lower the value,
-            the more robust it is, but will require much more computations.
+            the more robust it is (but will require much more computations).
 
     Returns:
         dict[int, dict[int, float]]:
@@ -147,61 +166,79 @@ def calculate_approximations(
                     for which holds :math:`p \ge p^*`,
                     where :math:`p^*` is the `probability_threshold`.
     """
-    approximations: Dict[int, Dict[int, float]] = defaultdict(lambda: dict())
-    if isfile(approximations_file):
-        with open(approximations_file, "rb") as saved_file:
-            approximations.update(load(saved_file))
-
-    total_approximations = sum([
+    approximations_amount = (lambda holder: sum(
         len(alpha_approximations)
-        for _, alpha_approximations in approximations.items()
-    ])
-    print(f"Total approximations {total_approximations} loaded.")
+        for alpha_approximations in holder.values()
+    ))
 
-    if total_approximations >= approximations_number:
+    total_approximations = approximations_amount(approximations)
+    print(f"Total approximations {total_approximations}.")
+    if total_approximations >= max_approximations_number:
         return dict(approximations)
 
     print(f"Finding linear approximations for the Heys cipher...")
-    for alpha_id, alpha in enumerate(alphas):
+    for alpha_id, alpha in enumerate(approximations.keys()):
         print(
             "Finding approximations for "
-            f"{alpha_id + 1}/{alphas.shape[0]} alpha..."
+            f"{alpha_id + 1}/{len(approximations.keys())} alpha..."
         )
+
         approximations[alpha].update(branch_bound(
             heys=heys,
             alpha=alpha,
             probability_threshold=probability_threshold,
         ))
-        total_approximations = sum([
-            len(alpha_approximations)
-            for _, alpha_approximations in approximations.items()
-        ])
-        print(f"Total approximations {total_approximations}.")
-        with open(approximations_file, "wb") as save_file:
-            dump(dict(approximations), save_file)
+        total_approximations = approximations_amount(approximations)
 
-        if total_approximations >= approximations_number:
+        print(f"Total approximations {total_approximations}.")
+        if total_approximations >= max_approximations_number:
             break
 
     return dict(approximations)
 
 
 def main():
-    alphas = array([
-        0b1111000000000000,
-        0b111100000000,
-        0b11110000,
-        0b1111,
-    ], dtype="uint16")
-    attack(
-        alphas=alphas,
-        approximations_file="heys-approximations-full.pkl",
-        keys_file="heys-keys.pkl",
-        approximations_number=100,
+    approximations_file = "heys-approximations-full.pkl"
+    key_candidates_file = "heys-keys.pkl"
+
+    heys_keys = array(
+        [0xafaf, 0x1234, 0x9998, 0x3457, 0x12, 0x1488, 0x7376],
+        dtype="uint16",
+    )
+    heys = Heys(sbox=S_BOX, keys=heys_keys)
+    opentexts = arange(start=0, stop=20000, dtype="uint16")
+    ciphertexts = heys.encrypt(message=opentexts)
+
+    approximations = {
+        0b1111000000000000: {},
+        0b111100000000: {},
+        0b11110000: {},
+        0b1111: {},
+    }
+
+    if isfile(approximations_file):
+        with open(approximations_file, "rb") as saved_file:
+            approximations = load(saved_file)
+
+    approximations, key_candidates = attack(
+        heys=heys,
+        data=(opentexts, ciphertexts),
+        approximations=approximations,
+        max_approximations_number=200,
         probability_threshold=5e-5,
         processes_number=6,
-        top_keys=200,
+        top_keys=100,
     )
+
+    with open(approximations_file, "wb") as save_file:
+        dump(dict(approximations), save_file)
+    with open(key_candidates_file, "wb") as save_file:
+        dump(dict(key_candidates), save_file)
+
+    print(list(reversed(sorted(
+        key_candidates.items(),
+        key=lambda element: element[1],
+    ))))
 
 
 if __name__ == "__main__":
